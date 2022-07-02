6f9c3215f159577df59b04067ce1818d3439d7eb
Nalain <possenalain21@gmail.com>
2022-07-02 13:19:58 +0300

    added a customer entity

---
+++ src/main/java/com/nalain/domain/Customer.java
@@ -0,0 +1,99 @@
+package com.nalain.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customer {
+
+    private String firsName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String addressLineOne;
+    private String addressLineTwo;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    Integer id;
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddressLineOne(String addressLineOne) {
+        this.addressLineOne = addressLineOne;
+    }
+
+    public void setAddressLineTwo(String addressLineTwo) {
+        this.addressLineTwo = addressLineTwo;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddressLineOne() {
+        return addressLineOne;
+    }
+
+    public String getAddressLineTwo() {
+        return addressLineTwo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
