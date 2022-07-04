b2964c8638ed8e7eb056996d277702d59eaddaba
Nalain <possenalain21@gmail.com>
2022-07-04 14:50:04 +0300

    added H2 database and spring data jpa dependencies

--- src/main/java/com/nalain/SpringMvcApplication.java
+++ src/main/java/com/nalain/SpringMvcApplication.java
@@ -2,12 +2,22 @@ package com.nalain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringMvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(SpringMvcApplication.class, args);
+
+		/*System.out.println("Beans *********");
+		System.out.println(ctx.getBeanDefinitionCount());
+
+		System.out.println("Names *******");
+
+		for(String bean: ctx.getBeanDefinitionNames())
+			System.out.println(bean);*/
 	}
 
 }
